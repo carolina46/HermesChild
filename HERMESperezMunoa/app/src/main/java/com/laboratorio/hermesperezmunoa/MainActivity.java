4e206d31d0eb1e643b127b794a264b3cd67be7c7
carolina <mariacarola46@hotmail.com>
2015-12-11 19:44:18 -0300

    Cambio del logotipo de cedica

--- HERMESperezMunoa/app/src/main/java/com/laboratorio/hermesperezmunoa/MainActivity.java
+++ HERMESperezMunoa/app/src/main/java/com/laboratorio/hermesperezmunoa/MainActivity.java
@@ -3,11 +3,28 @@ package com.laboratorio.hermesperezmunoa;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<String> childList=new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cargarNinos();
+    }
+
+
+    private void cargarNinos(){
+        childList.add("Marcos");
+        childList.add("Laura");
+        childList.add("Coca");
+        childList.add("Diego");
+        childList.add("Noe");
+        childList.add("Federico");
+        childList.add("Juan");
     }
 }
